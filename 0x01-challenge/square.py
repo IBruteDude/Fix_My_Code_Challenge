ae70aa6e695c9f85857b5445aeae62599802c4f6
IBruteDude <kamarf505@gmail.com>
2023-10-14 09:55:27 +0300

    Fixed what I could, AlHamdulillah

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+Square class
+"""
+
 
 class square():
+    """ Documentation """
     def __init__(self, *args, **kwargs):
+        """ Documentation """
         self.width = 0
         self.height = 0
         for key, value in kwargs.items():
@@ -12,9 +18,11 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ string representation of the square """
         return "{}/{}".format(self.width, self.height)
 
 
--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" 
+"""
 User class
 """
+
 
 class User():
     """ Documentation """
